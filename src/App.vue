36f4cd9634dbebb8a60e9f3f0b4ed20b99038e28
Michael Solone <michaelsolone@gmail.com>
2018-09-06 15:22:39 -0400

    Game board works and clicks

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="mine_sweeper">
+      <MineSweeper />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MineSweeper from './components/MineSweeper.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MineSweeper
   }
 }
 </script>
@@ -24,5 +26,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.mine_sweeper {
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/MineSweeper.vue
@@ -0,0 +1,87 @@
+<template>
+<section>
+    <h3>{{ message}}</h3>
+    <table class="game_board">
+        <tbody>
+            <tr v-for="(row, i) in board" v-bind:key="i">
+                <td class="squares" v-for="(col, j) in row"
+                 v-bind:key="j" 
+                 v-on:click="squareClicked(i,j)"
+            
+                 >
+                        {{ col }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+
+</template>
+
+<script>
+export default {
+  name: "MineSweeper",
+  data: function() {
+    return {
+      board: [],
+      gameId: 0,
+      message: "Let's Play MineSweeper"
+    };
+  },
+  mounted: function() {
+    this.createGame();
+  },
+  methods: {
+    createGame() {
+      const BASE_URL = "https://minesweeper-api.herokuapp.com/";
+      fetch(`${BASE_URL}games`, {
+        method: "POST",
+        body: JSON.stringify({ difficulty: 0 }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(newGame => {
+          console.log("Game", newGame);
+          this.board = newGame.board;
+          this.gameId = newGame.id;
+        });
+    },
+    squareClicked(i, j) {
+      console.log("row:", i, "col:", j);
+      const BASE_URL = "https://minesweeper-api.herokuapp.com/";
+      fetch(`${BASE_URL}games/${this.gameId}/check`, {
+        method: "POST",
+        body: JSON.stringify({ row: i, col: j }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(latestGame => {
+          console.log("Latest Game", latestGame);
+          if (latestGame.state === 'lost') {
+            this.board = latestGame.board;
+            this.message = 'You Lose!'
+          } else if (latestGame.state === 'won') {
+            this.board = latestGame.board;
+            this.message = 'You Win!'
+          } else {
+            this.board = latestGame.board;
+            this.message = 'Watch out for Bombs!'
+          }
+       
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+td {
+  border: 3px solid black;
+  height: 2em;
+  width: 2em;
+}
+</style>
